0c5404b7bde76ee25d2cc71ad19cfd0ac0283f58
SESURAJA <sesuraja771@gmail.com>
2024-03-30 08:04:52 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -112,21 +112,7 @@ async function fetchData(type = "skills") {
     skillsContainer.innerHTML = skillHTML;
     }
 
-function showProjects(projects) {
-    let projectsContainer = document.getElementById("projectsContainer");
-    let projectHTML = "";
-    projects.forEach(project => {
-        projectHTML += `
-        <div class="project">
-              <div class="info">
-                <h3>${project.title}</h3>
-                <p>${project.description}</p>
-                <a href="${project.url}" target="_blank" class="btn">View Project</a>
-              </div>
-            </div>`
-    });
-    projectsContainer.innerHTML = projectHTML;
-}
+
     
 
 
