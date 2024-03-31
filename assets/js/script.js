c1fc3b21956b99b80277d2abb3ea9f662616e795
SESURAJA <sesuraja771@gmail.com>
2024-03-31 11:49:58 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -109,7 +109,7 @@ function showProjects(projects) {
     projects.slice(0, 10).filter(project => project.category != "android").forEach(project => {
      projectHTML += `
         <div class="box tilt">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+      <img draggable="false" src="/assets/images/${project.image}.png" alt="project" />
       <div class="content">
         <div class="tag">
         <h3>${project.name}</h3>
@@ -124,7 +124,7 @@ function showProjects(projects) {
       </div>
     </div>`   
     });
-
+}
     projectsContainer.innerHTML = projectHTML;
     
     // <!-- tilt js effect starts -->
