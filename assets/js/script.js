c5475653bcd5f6fc44b1b3e2b2b16f67107ce372
SESURAJA <sesuraja771@gmail.com>
2025-12-19 18:13:03 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,7 +28,6 @@ $(document).ready(function () {
             }
         });
     });
-    
 
     // smooth scrolling
     $('a[href*="#"]').on('click', function (e) {
@@ -38,17 +37,20 @@ $(document).ready(function () {
         }, 500, 'linear')
     });
 
-    
-
-document.addEventListener('visibilitychange', function () {
-    if (document.visibilityState === "visible") {
-        document.title = "Portfolio | Sesu Raja";
-        $("link[rel='icon']").attr("href", "assets/images/favicon.png");
-    } else {
-        document.title = "Come Back To Portfolio";
-        $("link[rel='icon']").attr("href", "assets/images/favhand.png");
-    }
 });
+
+document.addEventListener('visibilitychange',
+    function () {
+        if (document.visibilityState === "visible") {
+            document.title = "Portfolio | Sesu Raja";
+            $("#favicon").attr("href", "assets/images/favicon.png");
+        }
+        else {
+            document.title = "Come Back To Portfolio";
+            $("#favicon").attr("href", "assets/images/favicon.png");
+        }
+    });
+
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
@@ -86,7 +88,6 @@ function showSkills(skills) {
 }
 
 
-
 fetchData().then(data => {
     showSkills(data);
 });
@@ -112,11 +113,24 @@ VanillaTilt.init(document.querySelectorAll(".tilt"), {
 // window.onload = fadeOut;
 // pre loader end
 
-
-
-
-
-    
+// disable developer mode
+document.onkeydown = function (e) {
+    if (e.keyCode == 123) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    }
+}
 
 
 
