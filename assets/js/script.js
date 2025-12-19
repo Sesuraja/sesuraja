7a8ab1d1342c5b83c69dc1d3b365f4bb6640ef17
SESURAJA <sesuraja771@gmail.com>
2025-12-19 17:33:46 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,7 +87,7 @@ function showSkills(skills) {
 
 
 
-fetchData().then(data => {
+fetchData("skills").then(data => {
     showSkills(data);
 });
 
@@ -118,17 +118,6 @@ VanillaTilt.init(document.querySelectorAll(".tilt"), {
 
     
 
-// Start of Tawk.to Live Chat
-var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-(function () {
-    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-    s1.async = true;
-    s1.src = 'https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
-    s1.charset = 'UTF-8';
-    s1.setAttribute('crossorigin', '*');
-    s0.parentNode.insertBefore(s1, s0);
-})();
-// End of Tawk.to Live Chat
 
 
 /* ===== SCROLL REVEAL ANIMATION ===== */
